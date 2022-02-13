0c7b2c489395c986f480c37b97afd508dc59254b
Pablo Villarroel <pablovillarroel135@gmail.com>
2022-02-13 14:39:52 -0300

    finish day 6 challenge

--- 06.js
+++ 06.js
@@ -1,25 +1,51 @@
-const almacen = {
-  estanteria1: {
-    producto1: "pan",
-    producto2: "sopa"
-  },
-  estanteria2: {
-    producto1: "mandarina",
-    producto2: "tomate"
-  },
-  producto1: "camiseta"
-}
+function contains(store, product) {
+  let cola = [];
+  cola.push(store);
 
-let cola = [];
-cola.push(almacen);
-
-while (cola.length > 0) {
-  const firstElement = cola[0];
-  for (const element in firstElement) {
-    const value = firstElement[element]
-    if (typeof value === "object") {
-      cola.push(value);
-    } else console.log(value);
+  while (cola.length > 0) {
+    const firstElement = cola[0];
+    for (const element in firstElement) {
+      const value = firstElement[element]
+      if (typeof value === "object") {
+        cola.push(value);
+      } else {
+        if (value === product) {
+          return true;
+        } 
+      }
+    }
+    cola.shift();
   }
-  cola.shift();
+  return false;
 }
+
+const almacen = {
+  'estanteria1': {
+    'cajon1': {
+      'producto1': 'coca-cola',
+      'producto2': 'fanta',
+      'producto3': 'sprite'
+    }
+  },
+  'estanteria2': {
+    'cajon1': 'vacio',
+    'cajon2': {
+      'producto1': 'pantalones',
+      'producto2': 'camiseta' // <- ¡Está aquí!
+    }
+  }
+}
+
+console.log(contains(almacen, 'camiseta')); // true
+
+const otroAlmacen = {
+  'baul': {
+    'fondo': {
+      'objeto': 'cd-rom',
+      'otro-objeto': 'disquette',
+      'otra-cosa': 'mando'
+    }
+  }
+}
+
+console.log(contains(otroAlmacen, 'gameboy')) // false
